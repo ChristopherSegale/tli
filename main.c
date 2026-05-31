d481e23b540bd4a4aaf8d0b9dd7b9bee19d1bb17
ChristopherSegale <ccs.segale@gmail.com>
2026-05-31 15:08:52 -0400

    Adding usage instructions for interpreter

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+void printUsage(char *programName) {
+  printf("Usage instructions for %s:\n", programName);
+  puts("-c compile file to byte code");
+  puts("-l load file(s)");
+  puts("-i launch interpreter");
+  puts("-h print usage flags");
+  puts("Running the program with no arguments will launch the interpreter");
+}
+
 int main(int argc, char *argv[]) {
-  puts("Trivial Lisp Interpreter");
+  printUsage(argv[0]);
   return 0;
 }
