58dbb301ec31b8ce730da787c5d99c7de9ea20f4
ChristopherSegale <ccs.segale@gmail.com>
2026-06-05 14:49:38 -0400

    Adding function declarations to create Trivial Lisp objects.

--- src/dataTypes.h
+++ src/dataTypes.h
@@ -1,6 +1,15 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
-enum dataTypes { t, nil, integer, decimal, array, structure, cons, list, function, symbol };
+enum dataType { t, nil, integer, decimal, array, structure, cons, list, function, symbol };
+
+struct TLObject {
+  enum dataType;
+  void *value;
+};
+
+struct TLObject *makeObject(enum dataType);
+void assignValue(enum dataType, struct TLObject **obj);
+void cleanTLObject(struct TLObject **obj);
 
 #endif
