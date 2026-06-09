def55fda41923ea944a448f27aab095b1a1fc941
ChristopherSegale <ccs.segale@gmail.com>
2026-06-09 17:12:25 -0400

    Adding functions for initializing and assigning values to Trivial Lisp objects in header file.

--- src/dataTypes.h
+++ src/dataTypes.h
@@ -9,7 +9,15 @@ struct TLObject {
 };
 
 struct TLObject *makeObject(enum dataType);
-void assignValue(enum dataType, struct TLObject **obj);
+int initValue(struct TLObject **obj, enum dataType);
+int assignInteger(struct TLObject **obj, int value);
+int assignDecimal(struct TLObject **obj, double value);
+int assignArray(struct TLObject **obj, struct TLArray *value);
+int assignStructure(struct TLObject **obj, struct TLStruct *value);
+int assignCons(struct TLObject **obj, struct TLCons *value);
+int assignList(struct TLObject **obj, struct TLList *value);
+int assignFunction(struct TLObject **obj, struct TLFunc *value);
+int assignSymbol(struct TLObject **obj, char *value);
 void cleanTLObject(struct TLObject **obj);
 
 #endif
