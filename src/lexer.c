c9bc41cdcda4698c4516ee397ab4a560e3621e9b
ChristopherSegale <ccs.segale@gmail.com>
2026-07-03 22:49:51 -0400

    Working on lexer.

--- src/lexer.c
+++ src/lexer.c
@@ -2,11 +2,96 @@
 #include <string.h>
 #include "lexer.h"
 
+/* characters have a 50 character limit */
+#define BUFFERSIZE 51
+
+enum state { comment, qt, bq, collect, read };
+
 struct lexeme *read(char *expression) {
-  struct lexeme *ast = malloc(sizeof(struct lexeme) * 512);
+  int size = strlen(expression);
+  struct lexeme *ast = malloc(sizeof(struct lexeme) * size);
   if (!ast)
     return NULL;
+  int i = 0, pairCount = 0, j = 0, k = 0; //j index for ast and k index for token
+  char buffer[BUFFERSIZE];
+  enum state st = read;
+  for(char c = *(expression + i), p = *(expression + i + 1); i < size; i++, c = *(expression + i)) {
+    int fail = parse(ast, c, p, buffer, &j, &k, &s, &pairCount);
+    if(fail) {
+      free(ast);
+      return NULL;
+    }
+    if(c == '#' || c == '\\')
+      i++;
+    if(i <= size)
+      p = *(expression + i + 1);
+  }
+  *(ast + j + 1) = makeEnd();
   return ast;
+}
+
+int parse(struct lexeme *tree, char lchar, char pchar, char *string, int *treeIndex, int *stringIndex, enum state *st, int *pc) {
+  int noAlloc = 0;
+  switch (*st) {
+  case read:
+    if(lchar == '(') {
+      *(tree + *treeIndex) = makeLP();
+      *(treeIndex)++;
+      *(pc)++;
+    }
+    else if(lchar == ')') {
+      *(tree + *treeIndex) = makeRP();
+      *(treeIndex)++;
+      *(pc)--;
+    }
+    else if(lchar == '"') {
+      *st = qt;
+    }
+    else if(lchar == ';') {
+      *st = comment;
+    }
+    else if(lchar == '#') {
+      *(tree + *treeIndex) = makeSharp(pchar);
+      *(treeIndex)++;
+    }
+    else if(lchar == '`') {
+      *(tree + *treeIndex) = makeBackQuote();
+      *(treeIndex)++;
+      *st = bq;
+    }
+    else if(lchar == ',') {
+      if(pchar == ' ')
+	return 1;
+      *(tree + *treeIndex) = makeComma();
+      *(treeIndex)++;
+    }
+    else if(lchar == ' ') {
+    }
+    else {
+      *(string + *stringIndex) = lchar;
+      *(stringIndex)++;
+      *st = collect;
+    }
+    break;
+  case qt:
+    if(lchar == '\\') {
+      if(pchar == '"') {
+	*(string + *stringIndex) = '"';
+      }
+      *(stringIndex)++;
+    }
+    else if(lchar == '"') {
+      if(pchar != ' ')
+	return 1;
+      *(string + *stringIndex) = '\0';
+      *st = read;
+      *stringIndex = 0;
+      *(tree + *treeIndex) = makeDoubleQuote(string, &noAlloc);
+      if(noAlloc)
+	return 1;
+    }
+    break;
+  }
 }
 
 struct lexeme makeLexeme(enum lexChars token) {
@@ -41,8 +126,16 @@ struct lexeme makeComma() {
   return makeLexeme(comma);
 }
 
-struct lexeme makeDoubleQuote() {
-  return makeLexeme(doubleQuote);
+struct lexeme makeDoubleQuote(char *token, int *fail) {
+  struct lexeme val = makeLexeme(doubleQuote);
+  int size = strlen(token);
+  val.data = malloc((sizeof(char) * size) + 1);
+  if (!(val.data)) {
+    *fail = 1;
+    return val;
+  }
+  strcpy(val.data, token);
+  return val;
 }
 
 struct lexeme makeNumber(char *token, int *fail) {
@@ -67,4 +160,8 @@ struct lexeme makeSymbol(char *token, int *fail) {
   }
   strcpy(val.data, token);
   return val;
+}
+
+struct lexeme makeEnd() {
+  return makeLexeme(end);
 }
