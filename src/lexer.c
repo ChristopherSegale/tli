48bf8c52ead14634f0f928417fef060a13e1aee5
ChristopherSegale <ccs.segale@gmail.com>
2026-07-05 19:13:36 -0400

    Finishing up lexer.

--- src/lexer.c
+++ src/lexer.c
@@ -1,105 +1,27 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "lexer.h"
 
-/* characters have a 50 character limit */
+/* symbols have a 50 character limit */
 #define BUFFERSIZE 51
 
-enum state { comment, qt, bq, collect, read };
+enum state { comment, sharpComment, qt, collect, reading };
 
-struct lexeme *read(char *expression) {
-  int size = strlen(expression);
-  struct lexeme *ast = malloc(sizeof(struct lexeme) * size);
-  if (!ast)
-    return NULL;
-  int i = 0, pairCount = 0, j = 0, k = 0; //j index for ast and k index for token
-  char buffer[BUFFERSIZE];
-  enum state st = read;
-  for(char c = *(expression + i), p = *(expression + i + 1); i < size; i++, c = *(expression + i)) {
-    int fail = parse(ast, c, p, buffer, &j, &k, &s, &pairCount);
-    if(fail) {
-      free(ast);
-      return NULL;
-    }
-    if(c == '#' || c == '\\')
-      i++;
-    if(i <= size)
-      p = *(expression + i + 1);
-  }
-  *(ast + j + 1) = makeEnd();
-  return ast;
-}
-
-int parse(struct lexeme *tree, char lchar, char pchar, char *string, int *treeIndex, int *stringIndex, enum state *st, int *pc) {
-  int noAlloc = 0;
-  int igq = 0;
-  switch (*st) {
-  case read:
-    if(lchar == '(') {
-      *(tree + *treeIndex) = makeLP();
-      *(treeIndex)++;
-      *(pc)++;
-    }
-    else if(lchar == ')') {
-      *(tree + *treeIndex) = makeRP();
-      *(treeIndex)++;
-      *(pc)--;
-    }
-    else if(lchar == '"') {
-      *st = qt;
-    }
-    else if(lchar == ';') {
-      *st = comment;
-    }
-    else if(lchar == '#') {
-      *(tree + *treeIndex) = makeSharp();
-      *(treeIndex)++;
-    }
-    else if(lchar == '`') {
-      *(tree + *treeIndex) = makeBackQuote();
-      *(treeIndex)++;
-      *st = bq;
-    }
-    else if(lchar == ',') {
-      if(pchar == ' ')
+int isNumber(char *string) {
+  int useDecimal = 0;
+  for(int i = 0; i < strlen(string); i++) {
+    char c = *(string + i);
+    if (c == '.') {
+      if(useDecimal)
 	return 1;
-      *(tree + *treeIndex) = makeComma();
-      *(treeIndex)++;
-    }
-    else if(lchar == ' ') {
-    }
-    else {
-      *(string + *stringIndex) = lchar;
-      *(stringIndex)++;
-      *st = collect;
-    }
-    break;
-  case qt:
-    if(lchar == '"') {
-      if(igq) {
-	*(tree + *treeIndex) = makeSubstring('"');
-	igq = 0;
-      }
-      else {
-	if(pchar != ' ')
-	  return 1;
-	*st = read;
-        *(tree + *treeIndex) = makeDoubleQuote();
-      }
-    }
-    else {
-      if(lchar == '\\') {
-	if(igq) {
-	  *(tree + *treeIndex) = makeSubstring('\\');
-	  igq = 0;
-	}
-	else
-	  igq = 1;
-      }
       else
-	*(tree + *treeIndex) = makeSubstring(lchar);
-    break;
+	useDecimal = 1;
+    }
+    else if(!isdigit(c))
+      return 1;
   }
+  return 0;
 }
 
 struct lexeme makeLexeme(enum lexChars token) {
@@ -116,10 +38,8 @@ struct lexeme makeRP() {
   return makeLexeme(rp);
 }
 
-struct lexeme makeSharp(char specialChar) {
-  struct lexeme val = makeLexeme(sharp);
-  *(val.data) = specialChar;
-  return val;
+struct lexeme makeSharp() {
+  return makeLexeme(sharp);
 }
 
 struct lexeme makeQuote() {
@@ -134,7 +54,7 @@ struct lexeme makeComma() {
   return makeLexeme(comma);
 }
 
-struct lexeme makeDoubleQuote(char *token, int *fail) {
+struct lexeme makeDoubleQuote() {
   return makeLexeme(doubleQuote);
 }
 
@@ -170,4 +90,136 @@ struct lexeme makeSymbol(char *token, int *fail) {
 
 struct lexeme makeEnd() {
   return makeLexeme(end);
+}
+
+int parse(struct lexeme *tree, char lchar, char pchar, char *string, int *treeIndex, int *stringIndex, enum state *st, int *pc) {
+  int noAlloc = 0;
+  int igq = 0;
+  int isPipe = 0;
+  switch (*st) {
+  case reading:
+    if(lchar == '(') {
+      *(tree + *treeIndex) = makeLP();
+      *(treeIndex)++;
+      *(pc)++;
+    }
+    else if(lchar == ')') {
+      *(tree + *treeIndex) = makeRP();
+      *(treeIndex)++;
+      *(pc)--;
+    }
+    else if(lchar == '"') {
+      *st = qt;
+    }
+    else if(lchar == ';') {
+      *st = comment;
+    }
+    else if(lchar == '#') {
+      if(pchar == '|')
+	*st = sharpComment;
+      else {
+	*(tree + *treeIndex) = makeSharp();
+	*(treeIndex)++;
+      }
+    }
+    else if(lchar == '`') {
+      *(tree + *treeIndex) = makeBackQuote();
+      *(treeIndex)++;
+    }
+    else if(lchar == ',') {
+      if(isspace(pchar))
+	return 1;
+      *(tree + *treeIndex) = makeComma();
+      *(treeIndex)++;
+    }
+    else if(isspace(lchar)) {
+    }
+    else {
+      *(string + *stringIndex) = lchar;
+      *(stringIndex)++;
+      *st = collect;
+    }
+    break;
+  case qt:
+    if(igq) {
+      *(tree + *treeIndex) = makeSubstring(lchar);
+      *(treeIndex)++;
+      igq = 0;
+    }
+    else {
+      if(lchar != '\\') {
+	if(lchar == '"') {
+	  *(tree + *treeIndex) = makeDoubleQuote();
+	  *(treeIndex)++;
+	  *st = reading;
+	}
+	else {
+	  *(tree + *treeIndex) = makeSubstring(lchar);
+	  *(treeIndex)++;
+	}
+      }
+      else
+	igq = 1;
+    }
+    break;
+  case collect:
+    if(!isspace(lchar)) {
+      if(*stringIndex < (BUFFERSIZE - 1)) {
+	*(string + *stringIndex) = lchar;
+	*(stringIndex)++;
+      }
+      else
+	return 1;
+    }
+    else {
+      if(isNumber(string))
+	*(tree + *treeIndex) = makeNumber(string, &noAlloc);
+      else 
+	*(tree + *treeIndex) = makeSymbol(string, &noAlloc);
+      if(noAlloc)
+	return 1;
+      *stringIndex = 0;
+      *treeIndex++;
+      *st = reading;
+    }
+    break;
+  case comment:
+    if(lchar == '\n')
+      *st = reading;
+    break;
+  case sharpComment:
+    if(lchar == '|')
+      isPipe = 1;
+    else if (lchar == '#') {
+      if(isPipe)
+	*st = reading;
+      isPipe = 0;
+    }
+    else {
+    }
+    break;
+  }
+}
+
+struct lexeme *read(char *expression) {
+  int size = strlen(expression);
+  struct lexeme *ast = malloc(sizeof(struct lexeme) * size);
+  if (!ast)
+    return NULL;
+  int i = 0, pairCount = 0, j = 0, k = 0; //j index for ast and k index for token
+  char buffer[BUFFERSIZE];
+  enum state st = reading;
+  for(char c = *(expression + i), p = *(expression + i + 1); i < size; i++, c = *(expression + i)) {
+    int fail = parse(ast, c, p, buffer, &j, &k, &st, &pairCount);
+    if(fail) {
+      free(ast);
+      return NULL;
+    }
+    if(c == '#' || c == '\\')
+      i++;
+    if(i < (size - 1))
+      p = *(expression + i + 1);
+  }
+  *(ast + j + 1) = makeEnd();
+  return ast;
 }
