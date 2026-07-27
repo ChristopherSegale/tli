4079ef696a4b6c037a97e6228c81dcf7e724755a
ChristopherSegale <ccs.segale@gmail.com>
2026-07-26 22:24:54 -0400

    Refactoring 'read' function so that buffer and string index are declared in stack.

--- src/lexer.c
+++ src/lexer.c
@@ -8,8 +8,7 @@
 /* symbols have a 50 character limit */
 #define BUFFERSIZE 51
 
-char buffer[BUFFERSIZE];
-int stringIndex = 0;
+//int stringIndex = 0;
 enum state { comment, sharpComment, qt, collect, hashCollect, reading };
 
 int isNumber(char *string);
@@ -25,16 +24,17 @@ struct lexeme makeSubstring(char token);
 struct lexeme makeNumber(char *token);
 struct lexeme makeSymbol(char *token);
 void addBranch(struct ast **tree, struct lexeme leaf);
-int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int *igq, int *isPipe);
+int parse(struct ast **tree, char *buffer, int lchar, int pchar, enum state *st, int *stringIndex, int *pc, int *igq, int *isPipe);
 
 struct ast *read(char *expression) {
   int size = strlen(expression);
   struct ast *tree = NULL;
   int i = 0, pairCount = 0, igq = 0, isPipe = 0;
   enum state st = reading;
-  stringIndex = 0;
+  int stringIndex = 0;
+  char buffer[BUFFERSIZE];
   for(int c = *(expression + i), p = *(expression + i + 1); i < size; i++, c = *(expression + i)) {
-    int fail = parse(&tree, c, p, &st, &pairCount, &igq, &isPipe);
+    int fail = parse(&tree, buffer, c, p, &st, &stringIndex, &pairCount, &igq, &isPipe);
     if(fail) {
       cleanAST(tree);
       printError();
@@ -57,7 +57,7 @@ struct ast *read(char *expression) {
   return tree;
 }
 
-int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int *igq, int *isPipe) {
+int parse(struct ast **tree, char *buffer, int lchar, int pchar, enum state *st, int *stringIndex, int *pc, int *igq, int *isPipe) {
   switch (*st) {
   case reading:
     if(lchar == '(') {
@@ -96,8 +96,8 @@ int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int 
     else if(isspace(lchar)) {
     }
     else {
-      buffer[stringIndex] = lchar;
-      stringIndex++;
+      *(buffer + *stringIndex) = lchar;
+      (*stringIndex)++;
       *st = collect;
     }
     break;
@@ -121,22 +121,22 @@ int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int 
     break;
   case collect:
     if(!isspace(lchar)) {
-      if(stringIndex < (BUFFERSIZE - 1)) {
-	buffer[stringIndex] = lchar;
-	stringIndex++;
+      if(*stringIndex < (BUFFERSIZE - 1)) {
+	*(buffer + *stringIndex) = lchar;
+	(*stringIndex)++;
       }
       else {
 	setError("Symbols cannot exceed 50 characters in length.", NULL, 0);
 	return 1;
       }
       if(lchar == ')' || lchar == EOF) {
-	if(stringIndex < (BUFFERSIZE - 1)) {
-	  buffer[stringIndex] = '\0';
+	if(*stringIndex < (BUFFERSIZE - 1)) {
+	  *(buffer + *stringIndex) = '\0';
 	  if(isNumber(buffer))
 	    addBranch(tree, makeNumber(buffer));
 	  else
 	    addBranch(tree, makeSymbol(buffer));
-	  stringIndex = 0;
+	  *stringIndex = 0;
 	  *st = reading;
 	}
 	else {
@@ -146,8 +146,8 @@ int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int 
       }
     }
     else {
-      if(stringIndex < BUFFERSIZE)
-	buffer[stringIndex] = '\0';
+      if(*stringIndex < BUFFERSIZE)
+	*(buffer + *stringIndex) = '\0';
       else {
 	setError("Symbols cannot exceed 50 characters in length.", NULL, 0);
 	return 1;
@@ -156,20 +156,20 @@ int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int 
 	addBranch(tree, makeNumber(buffer));
       else 
 	addBranch(tree, makeSymbol(buffer));
-      stringIndex = 0;
+      *stringIndex = 0;
       *st = reading;
     }
     break;
   case hashCollect:
     if(!isspace(lchar)) {
-      if(stringIndex < (BUFFERSIZE - 1)) {
-	buffer[stringIndex] = lchar;
-	stringIndex++;
+      if(*stringIndex < (BUFFERSIZE - 1)) {
+	*(buffer + *stringIndex) = lchar;
+	(*stringIndex)++;
 	if(lchar == '(') {
-	  if(stringIndex < BUFFERSIZE) {
-	    buffer[stringIndex] = '\0';
+	  if(*stringIndex < BUFFERSIZE) {
+	    *(buffer + *stringIndex) = '\0';
 	    addBranch(tree, makeSharp(buffer));
-	    stringIndex = 0;
+	    *stringIndex = 0;
 	    *(pc)++;
 	    *st = reading;
 	  }
@@ -185,14 +185,14 @@ int parse(struct ast **tree, int lchar, int pchar, enum state *st, int *pc, int 
       }
     }
     else {
-      if(stringIndex < BUFFERSIZE)
-	buffer[stringIndex] = '\0';
+      if(*stringIndex < BUFFERSIZE)
+	*(buffer + *stringIndex) = '\0';
       else {
 	setError("Symbols after # cannot exceed 50 characters in length.", NULL, 0);
 	return 1;
       }
       addBranch(tree, makeSharp(buffer));
-      stringIndex = 0;
+      *stringIndex = 0;
       *st = reading;
     }
     break;
