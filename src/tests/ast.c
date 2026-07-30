7ffaf15e299db75389c6147d5029d4e232621aad
ChristopherSegale <ccs.segale@gmail.com>
2026-07-30 01:09:06 -0400

    Adding additional tests for ast.

--- src/tests/ast.c
+++ src/tests/ast.c
@@ -25,6 +25,9 @@ void printLexeme(struct lexeme l) {
   case comma:
     puts(",");
     break;
+  case dot:
+    puts(".");
+    break;
   case doubleQuote:
     putchar('"');
     break;
@@ -57,8 +60,10 @@ void runLexerTests(char *t[], int size) {
   for(int i = 0; i < size; i++) {
     printf("Running test %d:\n", i + 1);
     struct ast *a = read(t[i]);
-    if(a)
+    if(a) {
       printAST(a);
+      free(a);
+    }
   }
 }
 
@@ -68,9 +73,12 @@ int main(int argc, char *argv[]) {
     ";;;Hello World Program\n(((defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n`',finished", //Too many left-parantheses
     ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))))\n`',finished", //Too many right-parantheses
     ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n`', finished",  //Space after comma
-    ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n#('a 'b 'c",    //Too many left-parantheses with sharp 
+    ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n#('a 'b 'c",    //Too many left-parantheses with sharp
+    "'Here 'are 'some 'symbols . 'Error 'here",                                                                                                                                   //Dot outside list
+    ").)",                                                                                                                                                                        //Dot in unbalanced list
     ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n`',finished",   //Success
-    ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n#('a 'b 'c)"    //Success with sharp balanced
+    ";;;Hello World Program\n(defun hello ()\n  #|\n  This is a comment.\n  (format t \"Hello World!~%\")\n  |#\n  (funcall #'write \"Hello \" 1 \"st World!\"))\n#('a 'b 'c)",   //Success with sharp balanced
+    "'(a . b)"                                                                                                                                                                    //Success with dotted pair
   };
   runLexerTests(tests, sizeof(tests) / sizeof(tests[0]));
   return 0;
