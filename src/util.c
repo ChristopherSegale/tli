56e4e3ea87ef03b882a221fe67b39d5e9e1044f0
ChristopherSegale <ccs.segale@gmail.com>
2026-08-15 15:25:17 -0400

    Refactoring code for 'isNumber' function.

--- src/util.c
+++ src/util.c
@@ -6,16 +6,18 @@ int isNumber(char *string) {
   int useDecimal = 0;
   for(int i = 0; i < strlen(string); i++) {
     char c = *(string + i);
-    if (i == 0 && c == '-') {
-    }
-    else if (c == '.') {
+    if (c == '.') {
       if(useDecimal)
 	return 0;
       else
 	useDecimal = 1;
     }
-    else if(!isdigit(c))
-      return 0;
+    else if(!isdigit(c)) {
+      if(i == 0 && c == '-') {
+      }
+      else
+	return 0;
+    }
   }
   return 1;
 }
