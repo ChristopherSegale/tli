a2b1a2f2037085114fff4378e118bcdbc15110fc
ChristopherSegale <ccs.segale@gmail.com>
2026-08-16 21:53:21 -0400

    Adding additional functions for strings.

--- src/TLString.c
+++ src/TLString.c
@@ -40,3 +40,46 @@ int pushTLChar(struct TLString *s, uint8_t c) {
     return 0;
   }
 }
+
+uint8_t getTLChar(struct TLString s, int pos) {
+  if(s.string) {
+    if(pos < s.size)
+      return *(s.string + pos);
+    else
+      setError("Index exceeds length of string", NULL, 0);
+  }
+  else
+    setError("String is empty.", NULL, 0);
+  return 0;
+}
+
+struct TLString TLSubstring(struct TLString s, int start, int end, int *fail) {
+  struct TLString val;
+  if(s.string) {
+    if(start < end && start >= 0 && end < s.size) {
+      if(end - start > 1) {
+	uint8_t buffer[end - start];
+	for(int i = 0, j = start; j < end; i++, j++)
+	  buffer[i] = *(s.string + j);
+      }
+      else {
+	setError("Difference between end and start needs to be greater than 1.", NULL, 0);
+	*fail = 1;
+      }
+    }
+    else {
+      if(start >= end)
+	setError("The start index needs to be less than the end index.", NULL, 0);
+      else if(start < 0)
+	setError("The start index needs to be greater than or equal to 0.", NULL, 0);
+      else if(end >= s.size)
+	setError("The end index needs to be less than the size of the string.", NULL, 0);
+      *fail = 1;
+    }
+  }
+  else {
+    setError("String is empty.", NULL, 0);
+    *fail = 1;
+  }
+  return val;
+}
